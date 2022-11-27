19bd46846787554f787589678b4f040f478b4271
Dmitrii <666666667@mail.ru>
2022-11-27 19:51:44 +0300

    понял в чем ошибка else не может стоять на 1 линии с оператором if

--- zadacha6/Program.cs
+++ zadacha6/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.Write("Введите число: ");
 int NumA = Convert.ToInt32(Console.ReadLine());
 if (NumA % 2==1)
-            {
-                Console.WriteLine("число нечетное");
-           
-            }
-            else
+{
+                Console.WriteLine("число нечетное");            
+                
+                }
+
+                else
             {
                 Console.WriteLine("Число четное");
             }
