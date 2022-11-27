c230bc6b25848a22c0ef9874ddd34fdcc6bd691f
Dmitrii <666666667@mail.ru>
2022-11-27 18:45:21 +0300

    Задача 3 выполнена

---
+++ zadacha4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int NumA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int NumB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int NumC = Convert.ToInt32(Console.ReadLine());
+
+int max = NumA;
+
+if (NumA>max) max=NumA;
+if (NumB>max) max=NumB;
+if (NumC>max) max=NumC;
+
+Console.WriteLine(max);
+
+
