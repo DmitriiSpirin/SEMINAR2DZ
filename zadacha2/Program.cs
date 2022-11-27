21e0c59fe470e7f34a3996d6a98989a161df679b
Dmitrii <666666667@mail.ru>
2022-11-27 18:14:02 +0300

    Задача 2 сделана, но не совсем понятно условие, текстом прописано одно, ниже немного другое

---
+++ zadacha2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число a: ");
+int NumA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int NumB = Convert.ToInt32(Console.ReadLine());
+if (NumA>NumB)
+{
+    Console.WriteLine ("a>b");
+}
+else
+{
+    Console.WriteLine ("a<b");
+    }
