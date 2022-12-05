b23bbef235089fbc90b4dc2bf4ed90c93cb07a79
Dmitrii <666666667@mail.ru>
2022-12-05 17:56:41 +0300

    zadacha vipolnena

---
+++ zadacha23/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+if (n>m)
+{
+    Console.WriteLine($"Ошибка ввода чисел, число n должно быть больше m");
+}
+Console.WriteLine("Результат: ");
+while (n<=m & count<=m)
+{
+    int stepen3 = n*n*n;
+    Console.WriteLine($"{stepen3}");
+    n++;
+    count++;
+}
+
+
