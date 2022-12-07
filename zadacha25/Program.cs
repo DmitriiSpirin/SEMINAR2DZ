3ffdeb6f3cd3bddfc775d2a43ef6714adc1e4fa5
Dmitrii <666666667@mail.ru>
2022-12-07 22:20:59 +0300

    zadacha vipolnena

---
+++ zadacha25/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Pow(n,m));
+*/
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+    int result = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        result = result * n;
+    }
+    Console.WriteLine(result);
+
