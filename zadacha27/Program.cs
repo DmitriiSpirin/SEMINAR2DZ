ec1ae90d1f69dd17ef59ef0fe087d1ca04d22fc1
Dmitrii <666666667@mail.ru>
2022-12-08 17:20:23 +0300

    zadacha vipolnena

---
+++ zadacha27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число n: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+while (n>0)
+{
+    sum=sum + n%10;
+    n/=10;
+}
+Console.WriteLine("Сумма цифр числа n: ");
+Console.WriteLine(sum);
