3f129923da6b6213d2454e6767beb20e58446d4c
Dmitrii <666666667@mail.ru>
2022-12-08 17:09:18 +0300

    zadacha vipolnena

---
+++ zadacha29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите размерность массива: ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+int[] FillingArray(int n){
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = rnd.Next(0,20);
+    }
+    return arr;
+}
+Console.WriteLine("Получился массив: ");
+int[] arr = FillingArray(j);
+Console.WriteLine(String.Join(",", arr));
