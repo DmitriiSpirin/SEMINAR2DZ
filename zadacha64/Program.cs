dd9bf8c5db90cb5d82e239539aee2daca2b5b30f
Dmitrii <666666667@mail.ru>
2022-12-26 18:10:06 +0300

    finished

---
+++ zadacha64/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"M = {m}, N = {n} ->");
+
+void PrintValue(int val, int max)
+
+{
+    if (val % 2 == 0)
+
+    {
+
+        if (val == max || val == max - 1) Console.Write(val);
+
+        else Console.Write(val + " , ");
+
+    }
+
+    if (val != max) PrintValue(val + 1, max);
+
+    else return;
+}
+
+if (m > n) PrintValue(n, m);
+
+else PrintValue(m, n);
+
+
---
+++ zadacha68/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int nod(int n, int m)
+{
+    if (m==0)
+        return n;
+    return nod(m,n%m);
+}
+Console.WriteLine ($"Наибольший общий делитель: {nod(n,m)}");
